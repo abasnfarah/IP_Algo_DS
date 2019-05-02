b08bd1970a4a546fceecea1d9e0bb17988fae354
Abas Farah <abasnfarah@gmail.com>
2019-05-02 11:46:48 +0000

    orginized File Structure

---
+++ Data_Structures/Heap/Python/heap.py
@@ -0,0 +1,15 @@
+class MaxHeap:
+    def __init__(self, collection=None):
+        self.__heap = []
+
+        if collections is not None:
+            for el in collection:
+                self.push(el)
+
+def left(x):
+    return x * 2
+def right(x):
+    return (x *2) + 1
+
+def max_Heapify(arr,largest): 
+    l = left(largestkk-00
