e15b34230d6bb90c236e05ca8ada69991348d6c5
Abas Farah <abasnfarah@gmail.com>
2019-07-03 13:52:16 +0000

    Binary Search Tree Methods for Traversal, isbinaryTree

--- Algo_DS_Python/Algo_DS_Python/data_structures/BST/BST.py
+++ Algo_DS_Python/Algo_DS_Python/data_structures/BST/BST.py
@@ -10,7 +10,8 @@
              / \        / \                            \
            1    3      5   7                            3
 """
-
+from Algo_DS_Python.data_structures.queue.queue import ListQueue
+import sys
 # Class For BST 
 # Methods for class :
 #   isEmpty, find, findMin, findmax, insert, delete, height, 
@@ -20,29 +21,10 @@
 # Node with a value and right and left children
 class Node:
     def __init__(self, val):
-        self._val = val
+        self.val = val
         #self.key = key
-        self._right= None
-        self._left= None
-
-    # Getter and Setters
-    def getVal(self):
-        return self._val
-
-    def getRight(self):
-        return self._right
-
-    def getLeft(self):
-        return self._left
-
-    def setVal(self, val):
-        self._val = val
-
-    def setRight(self, right):
-        self._right = right
-
-    def setLeft(self, left):
-        self._left = left
+        self.right= None
+        self.left= None
 
 class BST:
     tree = None
@@ -60,16 +42,16 @@ class BST:
 
     # This is a wrapper function that takes in just the val
     def insertWrapper(self, tree, val):
-        if( val > tree.getVal()):
-            if(tree.getRight() == None):
-                tree.setRight(Node(val))
+        if( val > tree.val):
+            if(tree.right == None):
+                tree.right = Node(val)
             else:
-                self.insertWrapper(tree.getRight(), val)
+                self.insertWrapper(tree.right, val)
         else:
-            if(tree.getLeft() == None):
-                tree.setLeft(Node(val))
+            if(tree.left == None):
+                tree.left = Node(val)
             else:
-                self.insertWrapper(tree.getLeft(), val)
+                self.insertWrapper(tree.left, val)
 
     # this takes O(height) time to insert as well due to binary 
     def insert(self, val):
@@ -83,16 +65,119 @@ class BST:
     def find(self, val):
         ptr = self.tree
         while(ptr != None):
-            if(ptr.getVal() == val):
+            if(ptr.val == val):
                 return True
             else:
-                ptr = ptr.getRight() if ptr.getVal() < val else ptr.getLeft()
+                ptr = ptr.right if ptr.val < val else ptr.left
         return False
 
 # This Prints the Tree Using In-Order-Traversal
-def printTree(tree):
-    if (tree != None):
-        printTree(tree.getLeft())
-        print(tree.getVal())
-        printTree(tree.getRight())
+def printTree(root):
+    if (root!= None):
+        printTree(root.left)
+        print(root.val)
+        printTree(root.right)
+
+# This function find the min element in BST
+def findMin(tree):
+    root = tree.getTree()
+    if(root == None):
+        print("ERROR: The tree is empty")
+        return None
+    while(root.left != None):
+        root = root.left
+
+    return root.val
+
+# This funciton finds the max element in BST
+def findMax(tree):
+    root = tree.getTree()
+    if(root == None):
+        print("Error: The tree is empty")
+        return None
+    while(root.right != None):
+        root = root.right
+
+    return root.val
+
+# This gets the root node out of BST then runs function
+def findHeight(tree):
+    root = tree.getTree()
+    return findHeightRecursive(root)
+
+# This function finds a height of a tree using recursion
+def findHeightRecursive(root):
+    if(root == None):
+        return -1
+
+    rightHeight = findHeightRecursive(root.right)
+    leftHeight = findHeightRecursive(root.left)
+
+    return max(rightHeight, leftHeight) + 1
+
+# This a BredthFirstSearch traversal printing every element tree by height
+def levelOrderTraversal(tree):
+    root = tree.getTree()
+    nodeQueue = ListQueue(root)
+
+    # Ends once queuue is empty and tree has been traversed
+    while(nodeQueue.isEmpty() == False):
+        ptr = nodeQueue.dequeue()
+        if(ptr.left != None and ptr.right != None):
+            nodeQueue.enqueue(ptr.left)
+            nodeQueue.enqueue(ptr.right)
+        if(ptr.left != None and ptr.right == None):
+            nodeQueue.enqueue(ptr.left)
+        elif(ptr.right != None and ptr.left == None):
+            nodeQueue.enqueue(ptr.right)
+        print(ptr.val)
+
+# These next three funcitons are different Depth-first Search traversals
+
+# Preorder Traversal gets the roots value then traverses the right sub tree then left subtree
+def preorder(root):
+    if(root != None):
+        print(root.val)
+        preorder(root.left)
+        preorder(root.right)
+def inorder(root):
+    if(root != None):
+        inorder(root.left)
+        print(root.val)
+        inorder(root.right)
+def inorderList(root):
+    if(root == None):
+        return [None]
+    L = inorderList(root.left)
+    R = inorderList(root.right)
+    if(L == [None] and R == [None]):
+        return [root.val]
+    elif(L == [None] and R != [None]):
+        return [root.val] + R
+    elif(L != [None] and R == [None]):
+        return L + [root.val]
+    else:
+        return L + [root.val] + R
+def postorder(root):
+    if(root != None):
+        postorder(root.left)
+        postorder(root.right)
+        print(root.val)
+
+# This function takes a root Node as imput and checks if it is a BST following the given invariant
+# That the left subtree L rooted at R is less then R.val and the right subtree is greater then R.val
+# This uses isSubtreeLesser and isSubtreeGreater as subroutines
+def isBinaryTree(root):
+    outputBoolean = True
+    if(root != None):
+        nodeList = inorderList(root)
+        x = -sys.maxsize
+
+        for i in range(0, len(nodeList)):
+            if(x < nodeList[i]):
+                x = nodeList[i]
+            else:
+                outputBoolean = False
+
+    return outputBoolean
 
